1f362634f44b3be19e66d3c504c8b34d0a6acace
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-06-21 14:21:56 -0400

    create reducer function for practice.

---
+++ src/hooks/Reducer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Reducer = () => {
+  return (
+    <>
+      <button>Increment age</button>
+      <p>Hello! your are.</p>
+    </>
+  );
+};
+
+export default Reducer;
