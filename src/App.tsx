734fa77fcee0f6c5c205bc3e3a7ca49604a32968
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-06-22 20:59:07 -0400

    feat:(fix file path issue) rewrite everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Reducer from "./hooks/Reducer";
 import "./App.css";
 
 function App() {
   return (
     <>
       <p>My Reat learning is starting now.</p>
+      <Reducer />
     </>
   );
 }
