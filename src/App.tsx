4aa7c1b979d477a6287c0be21a308e162c669a17
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-06-23 23:24:06 -0400

    feat:(update reducer function) using real world example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Reducer from "./hooks/Reducer";
 import "./App.css";
 
 function App() {
   return (
     <>
       <p>My Reat learning is starting now.</p>
-      <Reducer />
     </>
   );
 }
--- src/hooks/Reducer.jsx
+++ src/hooks/Reducer.jsx
@@ -1,28 +1,11 @@
-import React from "react";
-import { useReducer } from "react";
+import React from 'react'
 
-function reducer(state, action) {
-  if (action.type === "incremented_age") {
-    return {
-      age: state.age + 1,
-    };
-  }
-  throw Error("Unknown action.");
+function Reducer() {
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
-const Counter = () => {
-  const [state, dispatch] = useReducer(reducer, { age: 42 });
-  return (
-    <>
-      <button
-        onClick={() => {
-          dispatch({ type: "incremented_age" });
-        }}>
-        Increment age
-      </button>
-      <p>Hello! your are {state.age}.</p>
-    </>
-  );
-};
-
-export default Counter;
+export default Reducer
