ea32e7290fc7c9c5d00c6d9e8805a783b042de81
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-06-23 23:40:02 -0400

    feat(reducer.jsx): add important tags done.

--- src/hooks/Reducer.jsx
+++ src/hooks/Reducer.jsx
@@ -5,12 +5,28 @@ function Reducer() {
   const [count, setCount] = useState(0);
   const [userInput, setUserInput] = useState("");
   const [color, setColor] = useState(false);
-  return(
+  return (
     <>
-    
+      <main>
+        <input
+          type="text"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+        />
+        <br />
+        <br />
+        <p>{count}</p>
+        <section>
+          <button>-</button>
+          <button>+</button>
+          <button>Color</button>
+        </section>
+        <br />
+        <br />
+        <p>{userInput}</p>
+      </main>
     </>
-  )
-  
+  );
 }
 
 export default Reducer;
