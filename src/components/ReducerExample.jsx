4df40364c883684b12dcd49a09525563f9dd0197
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-06-26 00:16:09 -0400

    add reducer usestate hook

--- src/components/ReducerExample.jsx
+++ src/components/ReducerExample.jsx
@@ -1,9 +1,43 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import "./Reducer.css";
 
+// function ReducerExample() {
+//   const [count, setCount] = useState(0);
+//   const [userInput, setUserInput] = useState("");
+//   const [color, setColor] = useState(false);
+//   return (
+//     <>
+//       <main className="reduce-main">
+//         <input
+//           type="text"
+//           value={userInput}
+//           onChange={(e) => setUserInput(e.target.value)}
+//         />
+//         <br />
+//         <br />
+//         <p>{count}</p>
+//         <section className="reduce-section">
+//           <button onClick={() => setCount(count - 1)}>-</button>
+//           <button onClick={() => setCount(count + 1)}>+</button>
+//           <button onClick={() => setColor(!color)}>Color</button>
+//         </section>
+//         <br />
+//         <br />
+//         <p>{userInput}</p>
+//       </main>
+//     </>
+//   );
+// }
+
+// export default ReducerExample;
+
+// create a reducer function.
+
+const reducer = (state, action) => {};
+
 function ReducerExample() {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   const [userInput, setUserInput] = useState("");
   const [color, setColor] = useState(false);
   return (
