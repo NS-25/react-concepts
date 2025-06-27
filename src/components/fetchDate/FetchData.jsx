41a4764f79fb578cd2518d4594ab847d999e856f
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-06-27 11:21:11 -0400

    create fetch date files for react

---
+++ src/components/fetchDate/FetchData.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const FetchData = () => {
+  return <div className="fetchData">
+    <button>Generate Cat Fact</button>
+    <p></p>
+  </div>;
+};
+
+export default FetchData;
